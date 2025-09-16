f18dad9c1b8f6f1896fe84501f2a2b3a67bca606
OrnellaMarion <168301329+Ornella2006@users.noreply.github.com>
2025-09-17 00:17:21 +0300

    Initial commit - Projet Spring Boot

---
+++ src/test/java/com/example/gestionEntreprise/GestionEntrepriseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gestionEntreprise;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestionEntrepriseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
